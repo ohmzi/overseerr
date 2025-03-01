312cb0f2fac7261c8edff59f70297e696890e392
ohmz <omariqbal97@gmail.com>
2025-02-28 21:50:07 -0500

    removed StatusChecker due to popup not going away

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -166,7 +166,6 @@ const CoreApp: Omit<NextAppComponentType, 'origGetInitialProps'> = ({
                     applicationTitle={currentSettings.applicationTitle}
                   />
                 </Head>
-                <StatusChecker />
                 <ServiceWorkerSetup />
                 <UserContext initialUser={user}>{component}</UserContext>
               </ToastProvider>
